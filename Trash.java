7142851bfebd42cfff89b736a49ba807e5e09abb
Nikita23-m <84123462+Nikita23-m@users.noreply.github.com>
2021-05-13 14:33:29 +0530

    newtrash Added

--- Trash.java
+++ Trash.java
@@ -1,6 +1,7 @@
 public class Trash{
 public static void main(String[]args){
 System.out.println("my trash");
-
+System.out.println("my trash infinity");
+  System.out.println("my trash end");
 }
 } 
